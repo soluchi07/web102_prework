c9779a9ed4e2ebe6eabd83327a88d560ff643d83
Soluchi Fidel-Ibeabuchi <Soluchi.FIdel-Ibeabuchi.warwick.ac.uk>
2025-04-10 15:46:09 -0400

    challenge 5 done

--- index.js
+++ index.js
@@ -122,8 +122,12 @@ function filterUnfundedOnly() {
   deleteChildElements(gamesContainer);
 
   // use filter() to get a list of games that have not yet met their goal
+  let unFunded = GAMES_JSON.filter((game) => {
+    return game.pledged < game.goal;
+  });
 
   // use the function we previously created to add the unfunded games to the DOM
+  addGamesToPage(unFunded);
 }
 
 // show only games that are fully funded
@@ -131,8 +135,12 @@ function filterFundedOnly() {
   deleteChildElements(gamesContainer);
 
   // use filter() to get a list of games that have met or exceeded their goal
+  let beenFunded = GAMES_JSON.filter((game) => {
+    return game.pledged > game.goal;
+  });
 
   // use the function we previously created to add unfunded games to the DOM
+  addGamesToPage(beenFunded);
 }
 
 // show all games
@@ -140,6 +148,7 @@ function showAllGames() {
   deleteChildElements(gamesContainer);
 
   // add all games from the JSON data to the DOM
+  addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -148,7 +157,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
-
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
  * Skills used: template literals, ternary operator
